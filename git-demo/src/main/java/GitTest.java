517adbb4e2f6f7382fcd0eb117cba43f90abd722
Tjy <137208848@qq.com>
2022-06-06 23:50:33 +0800

    master

--- git-demo/src/main/java/GitTest.java
+++ git-demo/src/main/java/GitTest.java
@@ -2,6 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("master1");
         System.out.println("test1");
-
+        System.out.println("master2");
     }
 }
